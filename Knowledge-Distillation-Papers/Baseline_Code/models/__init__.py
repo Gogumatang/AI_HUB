bd2b46cdd4289aace44dd795503bf66da0650354
Gogumatang <82081350+Gogumatang@users.noreply.github.com>
2022-07-04 13:34:14 -0700

    Delete Knowledge-Distillation-Papers/Baseline_Code directory

--- Knowledge-Distillation-Papers/Baseline_Code/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .efficientnet import *
-
-
-
-def load_model(name, num_classes=10, pretrained=False, **kwargs):
-    model_dict = globals()
-    model = model_dict[name](pretrained=pretrained, num_classes=num_classes, **kwargs)
-    return model
